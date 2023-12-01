4e94e328019833675d357ec8d2f39c6858696588
jmb-oddbook <oddbook@posteo.net>
2023-12-01 09:12:01 +0100

    Added a template for AoC solutions.

---
+++ Code/template/solution.py
@@ -0,0 +1,25 @@
+"""
+Advent of Code
+"""
+
+# Frame for the first star
+def star1(data):
+    # code solution here
+    return data
+
+# Frame for second star
+def star2(data):
+    # code solution here
+    return data
+
+
+if __name__ == "__main__":
+    # data provided by AoC
+    input_data = open("input_data.txt").read
+
+    result = star1(input_data)
+    print(f"Result for Star 1: {result}")
+
+    result = star2(input_data)
+    print(f"Result for Star 2: {result}")
+
